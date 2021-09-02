4a77f8cdba05779115bea63977822415dc57186c
Alejandro Viveros <alejandroviverosm@gmail.com>
2021-09-01 22:44:18 -0400

    practicando ciclo while

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function multiply() {
     content += `</table>`
     document.getElementById("response").innerHTML = content;
 }
+
+function binary() {
+    let number = document.getElementById("txtNumber2").value;
+    let q = "";
+    do {
+        if (number % 2 == 0) {
+            q += "0";
+        } else {
+            q += "1";
+        }
+        number = parseInt(number/2); 
+
+    } while (number!=0);
+    document.getElementById("txtResponse").value = q;
+
+}
